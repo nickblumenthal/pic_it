d92fa01cab8e4c8ee5b3d1ce29e0205bde5e1b51
dcurletti <curlettid@gmail.com>
2015-03-01 17:50:37 -0800

    Added dynamic route changes
    Routes change templates now move in the correct direction

--- client/templates/transitions/transition.js
+++ client/templates/transitions/transition.js
@@ -11,11 +11,11 @@ var hooks = {
     // Make it visible
     $node.addClass('pt-page-current')
     // Adds transition effect
-    $node.addClass('pt-page-moveFromRight')
+    $node.addClass('pt-page-' + transitionDir('insert'))
 
     $node.on( animEndEventName, function () {
     	$node.off( animEndEventName );
-    	$node.removeClass('pt-page-moveFromRight')
+    	$node.removeClass('pt-page-' + transitionDir('insert'))
     })
   },
 
@@ -25,13 +25,15 @@ var hooks = {
     var $node = $(node);
     var that = this;
 
+
+    // if 	Router.current().url
     // $node.velocity('transition.slideRightBigOut', function () {
     //   $node.remove()
     //   that.transitioning = false;
     // })
 
 		// TEMP: Switch this to a Session variable
-		$node.addClass('pt-page-moveToLeft');
+		$node.addClass( 'pt-page-' + transitionDir('remove') );
 
 		$node.on( animEndEventName, function () {
 			$node.off( animEndEventName )
@@ -44,3 +46,25 @@ var hooks = {
 Template.transition.rendered = function() {
   this.firstNode.parentNode._uihooks = hooks;
 };
+
+var transitionDir = function ( action ) {
+	// Gets route client is navigating to
+	var toRoute = Router.current().url;
+	var direction;
+
+	if ( toRoute === "/" ) {
+		if ( action === "remove" ) {
+			direction = 'moveToRight';
+		} else {
+			direction = 'moveFromLeft';
+		}
+	} else {
+		if ( action === "remove" ) {
+			direction = 'moveToLeft';
+		} else {
+			direction = 'moveFromRight';
+		}
+	}
+
+	return direction;
+}
