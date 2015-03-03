3d4a7cbb2973c2548ab7df4f1656659c8c78c708
dcurletti <curlettid@gmail.com>
2015-03-02 23:55:20 -0800

    Added game template transitions, refactor game.html
    Added animations to changing game templates, refactored transition.js

--- client/templates/transitions/transition.js
+++ client/templates/transitions/transition.js
@@ -1,6 +1,7 @@
 // Can use a reactive var and allow transitions to continue when another is done
+// TEMP: Shameful, not DRY code. Refactor ASAP.
 
-
+// Hooks for initial page transition
 var hooks = {
   transitioning: false,
 
@@ -20,7 +21,6 @@ var hooks = {
     	$node.removeClass('pt-page-' + transitionDir('insert'))
     })
   },
-
 
   removeElement: function(node) {
     var remove;
@@ -43,6 +43,9 @@ var hooks = {
   }
 };
 
+
+// Refactor this into createTransitionHooks and remove openGames hidden class
+// Hooks for listing the games in home.html
 var gamesHooks = {
   transitioning: false,
 
@@ -74,56 +77,72 @@ var gamesHooks = {
   }
 };
 
-var gameTemplateHooks = {
-  transitioning: false,
+// Hooks for the templates inside of game.html
 
-  insertElement: function(node, next) {
-    var $node = $(node);
-    var that = this;
+var createTransitionHooks = function (transIn, transOut) {
+  var hooks = {
+    transitioning: false,
 
-    $node.hide();
+    insertElement: function(node, next) {
+      var $node = $(node);
+      var that = this;
+      var trans = 'transition.' + transIn;
 
-    $node.insertBefore(next);
+      $node.hide();
 
-    var insert = function () {
-      if (that.transitioning) {
-        Meteor.setTimeout( insert, 50 )
-      } else {
-        $node.velocity('transition.slideUpBigIn', { duration: 300, delay: 250, display: 'inline-block'})
-      }
-    };
-    insert();
-  },
+      $node.insertBefore(next);
 
-  removeElement: function(node) {
-    var $node = $(node);
-    var that = this;
+      var insert = function () {
+        if (that.transitioning) {
+          Meteor.setTimeout( insert, 50 )
+        } else {
+          $node.velocity( trans, { duration: 500, display: 'block'})
+        }
+      };
+      insert();
+    },
 
-    this.transitioning = true
-    $node.velocity('transition.slideDownBigOut', 300, function () {
-      $node.remove()
-      that.transitioning = false;
-    })
-  }
-};
+    removeElement: function(node) {
+      var $node = $(node);
+      var that = this;
+      var trans = 'transition.' + transOut;
+
+      this.transitioning = true
+      $node.velocity( trans, { 
+        duration: 500,
+        display: 'block', 
+        complete: function () {
+          $node.remove()
+          that.transitioning = false;
+      }})
+    }
+  };
+
+  return hooks
+}
  
+
+
+
+// Where hooks are applied
 Template.transition.rendered = function() {
 	var parentNode = this.firstNode.parentNode;
   var className = parentNode.className;
-
-  console.log(parentNode)
 
   // To distinguish with templates to give what hooks
 	if ( className === "games-container") {
 		parentNode._uihooks = gamesHooks;
 	} else if ( parentNode.id === "game-view" ) {
-    parentNode._uihooks = gameTemplateHooks;
+    parentNode._uihooks = createTransitionHooks('slideUpBigIn', 'slideDownBigOut');
+  } else if ( parentNode.id === "timer" ) {
+    parentNode._uihooks = createTransitionHooks('slideRightBigIn', 'slideRightBigOut')
   } else {
 		parentNode._uihooks = hooks;
 	}
 
 };
 
+// For figuring out which direction route changes should transition
 var transitionDir = function ( action ) {
 	// Gets route client is navigating to
 	var toRoute = Router.current().url;
